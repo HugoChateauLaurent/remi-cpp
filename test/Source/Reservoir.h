bec8b54c69b024ae177ee8c780fc27c4a439acdd
Tara Vanhatalo <tara.vanhatalo@gmail.com>
2023-11-16 14:23:09 +0100

    reservoir.h

--- test/Source/Reservoir.h
+++ test/Source/Reservoir.h
@@ -9,3 +9,22 @@
 */
 
 #pragma once
+
+#include <iostream>
+#include <Eigen/Dense>
+#include <random>
+
+class Reservoir
+{
+public:
+    Reservoir() = default;
+
+    void initialize();
+    VectorXd forward(VectorXd x);
+    VectorXd noise_gen(int size); 
+
+    void process(const float* inData, float* outData, int numSamples);
+    // void process(const float* inData, float param1, float param2, float* outData, int numSamples);
+// private:
+    
+};
